0a1e873f14e168fb084dad14ffa114bbd80a1c53
Pavan Rao <pavanraomr@gmail.com>
2020-08-04 20:12:20 -0400

    initial commit

---
+++ silly-name-generator/README.md
@@ -0,0 +1,17 @@
+### Book: impractical-python-projects 
+### Project #1: Generating Pseudonyms
+## A simple python program to generate nutty names by randomly combining first name and last names
+
+Load a list of first names
+Load a list of surnames
+Choose a first name at random
+Assign the name to a variable
+Choose a surname at random
+Assign the name to a variable
+Print the names to the screen in order and in red font
+Ask the user to quit or play again
+If user plays again:
+    repeat
+If user quits:
+    end and exit
+    
---
+++ silly-name-generator/pseudonyms.py
@@ -0,0 +1,65 @@
+import sys, random
+
+print('Welcome to silly-name-game!')
+print('How do you like this name?')
+
+# Load a list of first names
+first = ('Baby Oil', 'Bad News', 'Big Burps', "Bill 'Beenie-Weenie'",
+            "Bob 'Stinkbug'", 'Bowel Noises', 'Boxelder', "Bud 'Lite' ",
+            'Butterbean', 'Buttermilk', 'Buttocks', 'Chad', 'Chesterfield',
+            'Chewy', 'Chigger", "Cinnabuns', 'Cleet', 'Cornbread', 'Crab Meat',
+            'Crapps', 'Dark Skies', 'Dennis Clawhammer', 'Dicman', 'Elphonso',
+            'Fancypants', 'Figgs', 'Foncy', 'Gootsy', 'Greasy Jim', 'Huckleberry',
+            'Huggy', 'Ignatious', 'Jimbo', "Joe 'Pottin Soil'", 'Johnny',
+            'Lemongrass', 'Lil Debil', 'Longbranch', '"Lunch Money"',
+            'Mergatroid', '"Mr Peabody"', 'Oil-Can', 'Oinks', 'Old Scratch',
+            'Ovaltine', 'Pennywhistle', 'Pitchfork Ben', 'Potato Bug',
+            'Pushmeet','Rock Candy', 'Schlomo', 'Scratchensniff', 'Scut',
+            "Sid 'The Squirts'", 'Skidmark', 'Slaps', 'Snakes', 'Snoobs',
+            'Snorki', 'Soupcan Sam', 'Spitzitout', 'Squids', 'Stinky',
+            'Storyboard', 'Sweet Tea', 'TeeTee', 'Wheezy Joe',
+            "Winston 'Jazz Hands'", 'Worms')
+
+# Load a list of surnames
+last = ('Appleyard', 'Bigmeat', 'Bloominshine', 'Boogerbottom',
+        'Breedslovetrout', 'Butterbaugh', 'Clovenhoof', 'Clutterbuck',
+        'Cocktoasten', 'Endicott', 'Fewhairs', 'Gooberdapple', 'Goodensmith',
+        'Goodpasture', 'Guster', 'Henderson', 'Hooperbag', 'Hoosenater',
+        'Hootkins', 'Jefferson', 'Jenkins', 'Jingley-Schmidt', 'Johnson',
+        'Kingfish', 'Listenbee', "M'Bembo", 'McFadden', 'Moonshine', 'Nettles',
+        'Noseworthy', 'Olivetti', 'Outerbridge', 'Overpeck', 'Overturf',
+        'Oxhandler', 'Pealike', 'Pennywhistle', 'Peterson', 'Pieplow',
+        'Pinkerton', 'Porkins', 'Putney', 'Quakenbush', 'Rainwater',
+        'Rosenthal', 'Rubbins', 'Sackrider', 'Snuggleshine', 'Splern',
+        'Stevens', 'Stroganoff', 'Sugar-Gold', 'Swackhamer', 'Tippins',
+        'Turnipseed', 'Vinaigrette', 'Walkingstick', 'Wallbanger', 'Weewax',
+        'Weiners', 'Whipkey', 'Wigglesworth', 'Wimplesnatch', 'Winterkorn',
+        'Woolysocks')
+
+while True:
+    # Choose a first name at random
+    # random.choice Return a random element from the non-empty sequence seq
+    first_name = random.choice(first)
+    # Choose a lastname at random
+    last_name = random.choice(last)
+    # Print the names to the screen in order and in red font
+    # Use ANSI escape codes to print color - more details here
+    # https://ozzmaker.com/add-colour-to-text-in-python/
+    # \033[  Escape code, this is always the same
+    # 1 = Style, 1 for normal.
+    # 32 = Text colour, 32 for bright green.
+    # 40m = Background colour, 40 is for black.
+    print(f'\n\n\033[1;31;40m {first_name} {last_name}  \n\n') 
+
+    # print to sys.stderr for red color - this did not work
+    # print(f'{first_name} {last_name}  \n\n', file=sys.stderr)
+
+    # Ask the user to quit or play again
+    # If user plays again:
+    #     repeat
+    # If user quits:
+    #     end and exit
+    try_again = input('Try again? (Press Enter else n to quit)\n')
+    if (try_again.lower() == 'n'):
+        print('Thanks for playing!')
+        break
